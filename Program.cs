3f6f2348d5722f6519e38cfc9353c73cac01ea2b
ShinikumiAlex <144524709+ShinikumiAlex@users.noreply.github.com>
2024-12-11 17:31:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 2
+            string a = "abc";
+            string b = "cba";
+            string c = "bac";
+            Console.WriteLine($"{a}\n{b}\n{c}");
+            string d = a;
+            Console.WriteLine("");
+            a = b;
+            b = c;
+            c = d;
+            Console.WriteLine($"{a}\n{b}\n{c}");
+            Console.ReadKey();
+        }
+    }
+}
